2ea2ea320eb4f564a13b388c369a4ca5dffbfa9d
LuisVillarrealp <luvillarrealpi@uide.edu.ec>
2026-09-17 12:37:21 -0500

    Taller 1 Terminado hasta las 12:30

---
+++ Taller 1 Funciones/main.go
@@ -0,0 +1,85 @@
+package main
+
+import "fmt"
+
+func main() {
+	var usrOpcion string
+	fmt.Println("Ingresa tu opcion del 1 al 4 o 0 para salir ")
+	fmt.Println("La opcion 1 es para calcular la nota de estudiantes de un curso del 1 al 100")
+	fmt.Println("La opcion 2 es para calcular la suma desde 1 al numero elegido")
+	fmt.Println("La opcion 3 para convertir de Celsius a Farenheit")
+	fmt.Println("La opcion 4 es para convertir de Farenheit a Celsius")
+	fmt.Println("La opcion 0 o salir termina el programa")
+	fmt.Scan(&usrOpcion)
+	switch usrOpcion {
+	case usrOpcion == 1:
+		fmt.Println("Ingrese el numero de estudiantes" )
+		var numeroEstudiantes int
+		fmt.Scan(&numeroEstudiantes)
+	case usrOpcion==2:
+		fmt.Println("Ingresa un numero para calcular la suma")
+		var n int
+		fmt.Scan(&n)
+		fmt.Println("La suma de los numeros: ", totalSu)
+	case usrOpcion==3:
+		fmt.Println("Ingresa una temperatura en Celsius para convertir a Farenheit")
+		var temperaturaCelsius float64
+		fmt.Scan(&temperaturaCelsius)
+		fmt.Println(("La temperatura convertidad es ", opcion3(Farenheit)))
+	case usrOpcion==4 {
+		fmt.Println("Ingresa una temperatura en Farenheit para convertir a Celsius")
+		var temperaturaFarenheit float64
+		fmt.Scan(&temperaturaFarenheit)
+		fmt.Println("La temperatura convertidad es ",opcion4(Celsius) )
+	case usrOpcion==0 && usrOpcion=="salir":
+		break
+	default:
+		fmt.Println("Opcion Invalida")
+	}
+}
+
+func opcion(numeroEstudiantes int)float64{
+	nota :=[] float64
+	for contadorEstu := 1; contadorEstu <= numeroEstudiantes; contadorEstu++{
+		fmt.Println("Ingrese la nota del estudiante del 0 al 100",contadorEstu,":")
+		fmt.Scan(&nota)
+		SumaNotas:=0.0
+		for _,nota:=range notas {
+			sumaNotas +=nota
+		}
+	}
+	PromedioNotas=float64
+	PromedioNotas= sumaNotas/float64(len(notas))
+	
+	if promedioNotas >=70{
+		fmt.Println("Aprobado")
+	}else{
+		fmt.Println("Reprobado")
+	}
+	switch promedioNotas {
+	case promedioNotas>=70 && promedioNotas>=79:
+			fmt.Println("Satisfactory performance")
+	case promedioNotas>=80 && promedioNotas>=89:
+			fmt.Println( "Good performance")
+	case promedioNotas>=90 && promedioNotas>=100:
+			fmt.Println( "Good performance")
+	case promedioNotas>70:
+			fmt.Println("Needs improvement")
+	}
+
+}
+func opcion2(n int)int{
+	totalSu=:0
+	for contador := 1; contador <= n; contador++{
+		nota+=totalsu
+	}
+	return totalSu
+}
+func opcion3(temperaturaCelsius floar64)float64{
+	var Farenheit float64= ((temperaturaCelsius * (9/5)) + 32)
+	return Farenheit
+}
+func opcion4(temperaturaFarenheit floar64)float64{
+	var Celsius float64= ((temperaturaFarenheit − 32) × 5/9)
+	return Celsius
+}
